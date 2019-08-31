c108240c4c6cf6e78b39ff2bdab48371e6230289
metaleap <metaleap@users.noreply.github.com>
2019-09-01 00:04:22 +0200

    some more codegen-ing

--- libs/go/vscode.gen.go
+++ libs/go/vscode.gen.go
@@ -5,32 +5,33 @@ type MessageOptions struct {
 }
 
 type MessageItem struct {
-	Title             string `json:"title"`
-	IsCloseAffordance bool   `json:"isCloseAffordance,omitempty"`
+	Title string `json:"title"`
+	IsCloseAffordance bool `json:"isCloseAffordance,omitempty"`
 }
 
 type InputBoxOptions struct {
-	Value          string              `json:"value,omitempty"`
-	ValueSelection []int               `json:"valueSelection,omitempty"`
-	Prompt         string              `json:"prompt,omitempty"`
-	PlaceHolder    string              `json:"placeHolder,omitempty"`
-	Password       bool                `json:"password,omitempty"`
-	IgnoreFocusOut bool                `json:"ignoreFocusOut,omitempty"`
-	ValidateInput  func(string) string `json:"-"`
+	Value string `json:"value,omitempty"`
+	ValueSelection []int `json:"valueSelection,omitempty"`
+	Prompt string `json:"prompt,omitempty"`
+	PlaceHolder string `json:"placeHolder,omitempty"`
+	Password bool `json:"password,omitempty"`
+	IgnoreFocusOut bool `json:"ignoreFocusOut,omitempty"`
+	ValidateInput func(string) string `json:"-"`
 }
 
 type Window interface {
-	ShowErrorMessage1(message string, items []string) func(func(string))
-	ShowErrorMessage2(message string, options MessageOptions, items []string) func(func(string))
-	ShowErrorMessage3(message string, items []MessageItem) func(func(MessageItem))
-	ShowErrorMessage4(message string, options MessageOptions, items []MessageItem) func(func(MessageItem))
-	ShowInformationMessage1(message string, items []string) func(func(string))
-	ShowInformationMessage2(message string, options MessageOptions, items []string) func(func(string))
-	ShowInformationMessage3(message string, items []MessageItem) func(func(MessageItem))
-	ShowInformationMessage4(message string, options MessageOptions, items []MessageItem) func(func(MessageItem))
-	ShowWarningMessage1(message string, items []string) func(func(string))
-	ShowWarningMessage2(message string, options MessageOptions, items []string) func(func(string))
-	ShowWarningMessage3(message string, items []MessageItem) func(func(MessageItem))
-	ShowWarningMessage4(message string, options MessageOptions, items []MessageItem) func(func(MessageItem))
-	ShowInputBox(options InputBoxOptions) func(func(string))
+	ShowErrorMessage1(message string, items []string, ) func(func(string))
+	ShowErrorMessage2(message string, options MessageOptions, items []string, ) func(func(string))
+	ShowErrorMessage3(message string, items []MessageItem, ) func(func(MessageItem))
+	ShowErrorMessage4(message string, options MessageOptions, items []MessageItem, ) func(func(MessageItem))
+	ShowInformationMessage1(message string, items []string, ) func(func(string))
+	ShowInformationMessage2(message string, options MessageOptions, items []string, ) func(func(string))
+	ShowInformationMessage3(message string, items []MessageItem, ) func(func(MessageItem))
+	ShowInformationMessage4(message string, options MessageOptions, items []MessageItem, ) func(func(MessageItem))
+	ShowWarningMessage1(message string, items []string, ) func(func(string))
+	ShowWarningMessage2(message string, options MessageOptions, items []string, ) func(func(string))
+	ShowWarningMessage3(message string, items []MessageItem, ) func(func(MessageItem))
+	ShowWarningMessage4(message string, options MessageOptions, items []MessageItem, ) func(func(MessageItem))
+	ShowInputBox(options InputBoxOptions, ) func(func(string))
 }
+
