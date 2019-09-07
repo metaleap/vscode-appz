34f2fbdf3c7936570531698d81954e2a8ec58dd4
metaleap <metaleap@users.noreply.github.com>
2019-09-07 22:47:59 +0200

    IPC callbacks.. fun!

--- libs/go/basics.go
+++ libs/go/basics.go
@@ -67,18 +67,37 @@ func Via(stdin io.Reader, stdout io.Writer) Protocol {
 	return me
 }
 
-func (me *impl) loop() {
+func (me *impl) nextFuncId() string {
+	me.callbacks.counter++
+	return strconv.FormatUint(me.callbacks.counter, 36)
+}
+
+func (me *impl) loopReadln() {
 	for me.readln.Scan() {
 		if jsonmsg := strings.TrimSpace(me.readln.Text()); jsonmsg != "" {
 			var msg msgIncoming
 			err := json.Unmarshal([]byte(jsonmsg), &msg)
 			if err == nil {
 				me.callbacks.Lock()
-				cb := me.callbacks.waiting[msg.Callback]
+				cb, fn := me.callbacks.waiting[msg.Callback], me.callbacks.other[msg.Callback]
 				delete(me.callbacks.waiting, msg.Callback)
+				delete(me.callbacks.other, msg.Callback)
 				me.callbacks.Unlock()
 				if cb != nil {
 					cb(msg.Payload, msg.IsFail)
+				} else if fn != nil {
+					args, ok := msg.Payload.([]Any)
+					if !ok {
+						args = []Any{msg.Payload}
+					}
+					ret, ok := fn(args...)
+					me.send(&msgOutgoing{
+						Callback: msg.Callback,
+						Payload: map[string]Any{
+							"ret": ret,
+							"ok":  ok,
+						},
+					}, nil)
 				}
 			}
 			if err != nil {
@@ -86,11 +105,6 @@ func (me *impl) loop() {
 			}
 		}
 	}
-}
-
-func (me *impl) nextFuncId() string {
-	me.callbacks.counter++
-	return strconv.FormatUint(me.callbacks.counter, 36)
 }
 
 func (me *impl) send(msg *msgOutgoing, on func(Any, bool)) {
@@ -110,6 +124,6 @@ func (me *impl) send(msg *msgOutgoing, on func(Any, bool)) {
 		OnError(me, err, "")
 	}
 	if startloop {
-		me.loop()
+		me.loopReadln()
 	}
 }
