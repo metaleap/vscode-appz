e03a27f0c7b01421ad05910f5732902c5e676531
metaleap <metaleap@users.noreply.github.com>
2019-09-07 10:53:00 +0200

    gen msgOutgoings

---
+++ libs/go/basics.go
@@ -0,0 +1,23 @@
+package vscode
+
+type impl struct {
+}
+
+type msgOutgoing struct {
+	Ns       string                 `json:"ns"`   // eg. 'window'
+	Name     string                 `json:"name"` // eg. 'ShowInformationMessage3'
+	Payload  map[string]interface{} `json:"payload"`
+	Callback int                    `json:"callback"`
+}
+
+type msgIncoming struct {
+	Callback int           `json:"andThen"`
+	Payload  []interface{} `json:"payload"`
+}
+
+func New() Window {
+	return nil
+}
+
+func (me *impl) send(msg *msgOutgoing, on func(...interface{})) {
+}
--- src/extension.ts
+++ src/extension.ts
@@ -4,6 +4,7 @@ export function activate(ctx: vscode.ExtensionContext) {
 	ctx.subscriptions.push(vscode.commands.registerCommand('vsc_appz.main', () => {
 		vscode.window.showInformationMessage("Hi there")
 	}))
+
 }
 
 // this method is called when your extension is deactivated
