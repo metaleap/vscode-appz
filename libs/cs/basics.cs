ab1d7f6228b1d56d739000905485f31812b2d243
metaleap <metaleap@users.noreply.github.com>
2019-09-08 23:21:48 +0200

    enuff for now

--- libs/cs/basics.cs
+++ libs/cs/basics.cs
@@ -3,26 +3,24 @@ namespace Vscode {
     using System.IO;
     using System.Collections.Concurrent;
     using System.Collections.Generic;
+    using Newtonsoft.Json;
 
-    internal class msgToVsc {
+    internal partial class msgToVsc {
         internal string ns;
         internal string name;
         internal Dictionary<string, object> payload;
         internal string andThen;
     }
 
-    internal class msgFromVsc {
-        internal string andThen;
-        internal object payload;
-        internal bool failed;
+    internal partial class msgFromVsc {
+        internal string andThen = "";
+        internal object payload = null;
+        internal bool failed = false;
     }
 
-    public static class New {
-        public static IProtocol Vsc(this TextReader stdin, TextWriter stdout) {
-            return null;
-        }
-        public static IProtocol Vsc(this TextWriter stdout, TextReader stdin) {
-            return null;
+    public static class Via {
+        public static IProtocol InOut(TextReader stdIn = null, TextWriter stdOut = null) {
+            return new impl(stdIn, stdOut);
         }
     }
 
@@ -33,33 +31,39 @@ namespace Vscode {
         internal bool looping = false;
         internal uint counter = 0;
         internal readonly ConcurrentDictionary<string, Action<object>> cbWaiting = new ConcurrentDictionary<string, Action<object>>();
-        internal readonly ConcurrentDictionary<string, Func<object, (object, bool)>> cbOther = new ConcurrentDictionary<string, Func<object, (object, bool)>>();
+        internal readonly ConcurrentDictionary<string, Func<object[], (object, bool)>> cbOther = new ConcurrentDictionary<string, Func<object[], (object, bool)>>();
 
-        internal impl(TextReader stdin, TextWriter stdout) =>
-            (stdIn,stdOut) = (stdin,stdout);
+        internal impl(TextReader stdIn, TextWriter stdOut) =>
+            (this.stdIn, this.stdOut) = (stdIn ?? Console.In, stdOut ?? Console.Out);
 
-        internal string nextFuncId() {
-            lock (this) {
-                counter++;
-                return counter.ToString();
-            }
-        }
+        internal string nextFuncId() =>
+            (++counter).ToString();
 
         internal void loopReadln() {
             while (true) {
                 var jsonmsg = stdIn.ReadLine().Trim();
                 if (!string.IsNullOrEmpty(jsonmsg)) {
-                    msgFromVsc msg = null; // TODO: deserialize
+                    var msg = msgFromVsc.parse(jsonmsg);
                     Action<object> cb = null;
-                    Func<object, (object, bool)> fn = null;
+                    Func<object[], (object, bool)> fn = null;
                     lock (this) {
-                        cbWaiting.TryRemove(msg.andThen, out cb);
-                        cbOther.TryRemove(msg.andThen, out fn);
+                        _ = cbWaiting.TryRemove(msg.andThen, out cb);
+                        _ = cbOther.TryRemove(msg.andThen, out fn);
                     }
                     if (cb != null && !msg.failed)
                         cb(msg.payload);
                     else if (fn != null) {
-
+                        var args = msg.payload as object[];
+                        if (args == null)
+                            args = new object[] { msg.payload };
+                        var (ret, ok) = fn(args);
+                        send(new msgToVsc() {
+                            andThen = msg.andThen,
+                            payload = new Dictionary<string, object> () {
+                                ["ret"] = ret,
+                                ["ok"] = ok,
+                            },
+                        }, null);
                     }
                 }
             }
@@ -76,9 +80,72 @@ namespace Vscode {
                     cbWaiting[msg.andThen] = on;
                 }
             }
-            stdOut.WriteLine("{}");
+            stdOut.WriteLine(msg.ToString());
             if (startloop)
                 loopReadln();
+        }
+    }
+
+    internal partial class msgFromVsc {
+        internal static msgFromVsc parse(string jsonSrc) {
+
+            var me = new msgFromVsc();
+            JsonTextReader reader = new JsonTextReader(new StringReader(jsonSrc));
+            if ((!reader.Read()) || (reader.Value != null) || reader.TokenType != JsonToken.StartObject)
+                throw err();
+            while(reader.Read()) {
+                object name = reader.Value;
+                if (reader.TokenType == JsonToken.EndObject)
+                    break;
+                if (reader.TokenType != JsonToken.PropertyName || name == null || !reader.Read())
+                    throw err();
+                else
+                    switch ((string)name) {
+                        case "failed":
+                            me.failed = (bool)(reader.Value);
+                            break;
+                        case "andThen":
+                            me.andThen = (string)reader.Value;
+                            break;
+                        case "payload":
+                            throw err();
+                            break;
+                        default:
+                            throw err();
+                    }
+            }
+            return me;
+            Exception err() => new FormatException(jsonSrc);
+        }
+    }
+
+    internal partial class msgToVsc {
+        public override string ToString() {
+            var w = new StringWriter();
+            var jw = new JsonTextWriter(w);
+            jw.WriteStartObject();
+            if (!string.IsNullOrEmpty(ns)) {
+                jw.WritePropertyName("ns");
+                jw.WriteValue(ns);
+            }
+            if (!string.IsNullOrEmpty(name)) {
+                jw.WritePropertyName("name");
+                jw.WriteValue(name);
+            }
+            if (payload != null) {
+                jw.WriteStartObject();
+                foreach (var kvp in payload) {
+                    jw.WritePropertyName(kvp.Key);
+                    jw.WriteValue(kvp.Value);
+                }
+                jw.WriteEndObject();
+            }
+            if (!string.IsNullOrEmpty(andThen)) {
+                jw.WritePropertyName("andThen");
+                jw.WriteValue(andThen);
+            }
+            jw.WriteEndObject();
+            return w.ToString();
         }
     }
 
