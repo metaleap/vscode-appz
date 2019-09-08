4653c99d291903419d80cdabdf3506dbab8e4cb4
metaleap <metaleap@users.noreply.github.com>
2019-09-08 18:19:03 +0200

    wholesome hackeries for utmost smoothishnesses

--- demos/go-vsc-appz-demo/main.go
+++ demos/go-vsc-appz-demo/main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"os"
+
 	vsc "github.com/metaleap/vscode-appz/libs/go"
 )
 
 func main() {
+	greethow, greetname := "Hi", "World"
+	if len(os.Args) > 1 {
+		if greetname = os.Args[1]; len(os.Args) > 2 {
+			greethow = os.Args[2]
+		}
+	}
+
 	vscwin := vsc.Via(nil, nil).Window()
-	vscwin.ShowInformationMessage1("Hi World", []string{"btn1", "btn2"},
+	vscwin.ShowInformationMessage1(greethow+" "+greetname, []string{"btn1", "btn2"},
 		func(pick string) {
 			if pick == "btn1" || pick == "btn2" {
 				vscwin.ShowInformationMessage1("You picked: "+pick, nil, nil)
--- src/strvarinterp.ts
+++ src/strvarinterp.ts
@@ -1,66 +1,146 @@
+import * as node_path from 'path'
+
 import * as vsc from 'vscode'
+
+
+export enum Std {
+    workspaceFolder = "workspaceFolder",
+    workspaceFolderBasename = "workspaceFolderBasename",
+    file = "file",
+    relativeFile = "relativeFile",
+    relativeFileDirname = "relativeFileDirname",
+    fileBasename = "fileBasename",
+    fileBasenameNoExtension = "fileBasenameNoExtension",
+    fileDirname = "fileDirname",
+    fileExtname = "fileExtname",
+    cwd = "cwd",
+    lineNumber = "lineNumber",
+    selectedText = "selectedText",
+    execPath = "execPath",
+}
 
 
 export function Interpolate(str: string): Thenable<string> {
     const idx1 = str.lastIndexOf('${')
-    if (idx1 >= 0) {
-        const idx2 = str.indexOf('}', idx1)
-        if (idx2 >= 0) {
-            const from = (_: Thenable<any>) => _.then(
-                (ret: any) =>
-                    (ret === undefined) ? Promise.reject() : Interpolate(str.slice(0, idx1) + ret + str.slice(idx2 + 1)),
-                (err: any) =>
-                    Promise.reject(err)
-            )
+    if (idx1 < 0)
+        return Promise.resolve(str)
+    const idx2 = str.indexOf('}', idx1)
+    if (idx2 < 0)
+        return Promise.resolve(str)
 
-            const expr = str.slice(idx1 + 2, idx2)
-            let newval = ""
+    const fromProm = (_: Thenable<any>) => _.then(
+        (ret: any) =>
+            (ret === undefined) ? Promise.reject() : Interpolate(str.slice(0, idx1) + ret + str.slice(idx2 + 1)),
+        (err: any) =>
+            Promise.reject(err)
+    )
+    const expr = str.slice(idx1 + 2, idx2)
+    let newval = ""
 
-            if (expr.startsWith("env:"))
-                newval = process.env[expr.slice("env:".length)]
+    if (expr.startsWith("env:"))
+        newval = process.env[expr.slice("env:".length)]
 
-            else if (expr.startsWith("config:"))
-                newval = vsc.workspace.getConfiguration().get(expr.slice("config:".length))
+    else if (expr.startsWith("config:"))
+        newval = vsc.workspace.getConfiguration().get(expr.slice("config:".length))
 
-            else if (expr.startsWith("command:")) {
-                return from(vsc.commands.executeCommand(expr.slice("command:".length)))
+    else if (expr.startsWith("command:"))
+        return fromProm(vsc.commands.executeCommand(expr.slice("command:".length)))
 
-            } else if (expr.startsWith("input:")) {
-
-            } else if (expr === "workspaceFolder" || expr.startsWith("workspaceFolder:")) {
-
-            } else if (expr === "workspaceFolderBasename" || expr.startsWith("workspaceFolderBasename:")) {
-
-            } else if (expr === "file" || expr.startsWith("file:")) {
-
-            } else if (expr === "relativeFile" || expr.startsWith("relativeFile:")) {
-
-            } else if (expr === "relativeFileDirname" || expr.startsWith("relativeFileDirname:")) {
-
-            } else if (expr === "fileBasename" || expr.startsWith("fileBasename:")) {
-
-            } else if (expr === "fileBasenameNoExtension" || expr.startsWith("fileBasenameNoExtension:")) {
-
-            } else if (expr === "fileDirname" || expr.startsWith("fileDirname:")) {
-
-            } else if (expr === "fileExtname" || expr.startsWith("fileExtname:")) {
-
-            } else if (expr === "cwd" || expr.startsWith("cwd:")) {
-
-            } else if (expr === "lineNumber" || expr.startsWith("lineNumber:")) {
-
-            } else if (expr === "selectedText" || expr.startsWith("selectedText:")) {
-
-            } else if (expr === "execPath" || expr.startsWith("execPath:")) {
-
-            } else
-                return from(vsc.window.showInputBox({ ignoreFocusOut: true, prompt: "Enter the value for `${" + expr + "}`" }))
-
-            str = str.slice(0, idx1) + newval + str.slice(idx2 + 1)
+    else if (expr.startsWith("pick:")) {
+        const items = expr.slice("pick:".length).split('|')
+        if (items && items.length) {
+            let prompt = undefined
+            const colon = items[0].indexOf(':')
+            if (colon > 0)
+                [prompt, items[0]] = [items[0].slice(0, colon), items[0].slice(colon + 1)]
+            return fromProm(vsc.window.showQuickPick(items, { ignoreFocusOut: true, placeHolder: prompt }))
         }
     }
 
-    return Promise.resolve<string>(
-        (str.indexOf('${') < 0) ? str : Interpolate(str)
-    )
+    else if (expr.startsWith("input:"))
+        return fromProm(vsc.window.showInputBox({ ignoreFocusOut: true, prompt: expr.slice("input:".length) }))
+
+    else {
+        const fileordirstuff = (name: string): string => {
+            const eds = [vsc.window.activeTextEditor].concat(...vsc.window.visibleTextEditors.filter(_ => _ !== vsc.window.activeTextEditor)).filter(_ => _ ? true : false)
+            if (eds && eds.length)
+                for (const ed of eds)
+                    if (ed && ed.document && ed.document.uri && ed.document.uri.fsPath) {
+                        let wsf = vsc.workspace.workspaceFolders.find(_ => {
+                            let wsfuri = _.uri ? _.uri.toString() : null
+                            if (wsfuri && !wsfuri.endsWith('/')) wsfuri += '/'
+                            return wsfuri && ed.document.uri.toString().startsWith(wsfuri)
+                        })
+                        const curfilepath = (vsc.window.activeTextEditor && vsc.window.activeTextEditor.document && vsc.window.activeTextEditor.document.uri && vsc.window.activeTextEditor.document.uri.fsPath) ? vsc.window.activeTextEditor.document.uri.fsPath : null
+
+                        let affix = ""
+                        const idx = name.indexOf(':')
+                        if (idx > 0)
+                            [name, affix] = [name.slice(0, idx), name.slice(idx + 1)]
+                        if (affix && affix.length) {
+                            const oldwsf = wsf
+                            if (!(wsf = vsc.workspace.workspaceFolders.find(_ => _.name === affix)))
+                                wsf = oldwsf
+                        }
+
+                        if (wsf && ((!wsf.uri) || (!wsf.uri.fsPath)))
+                            wsf = null
+
+                        if (wsf && name === Std.workspaceFolder)
+                            return wsf.uri.fsPath
+                        else if (wsf && name === Std.workspaceFolderBasename)
+                            return node_path.basename(wsf.uri.fsPath)
+                        else if (curfilepath && name === Std.file) {
+                            return curfilepath
+                        } else if (curfilepath && wsf && name === Std.relativeFile)
+                            return node_path.relative(wsf.uri.fsPath, curfilepath)
+                        else if (curfilepath && wsf && name === Std.relativeFileDirname)
+                            return node_path.relative(wsf.uri.fsPath, node_path.dirname(curfilepath))
+                        else if (curfilepath && name === Std.fileBasename)
+                            return node_path.basename(curfilepath)
+                        else if (curfilepath && name === Std.fileBasenameNoExtension) {
+                            const fpbase = node_path.basename(curfilepath)
+                            const fpext = node_path.extname(fpbase)
+                            return fpext ? fpbase.slice(0, fpbase.length - fpext.length) : fpbase
+                        } else if (curfilepath && name === Std.fileDirname)
+                            return node_path.dirname(curfilepath)
+                        else if (curfilepath && name === Std.fileExtname)
+                            return node_path.extname(curfilepath)
+                        else if (name === Std.cwd)
+                            return process.cwd()
+                        else if (name === Std.lineNumber)
+                            return (ed.selection.active.line + 1).toString()
+                        else if (name === Std.selectedText)
+                            return ed.document.getText(ed.selection)
+                        else if (name === Std.execPath)
+                            return process.argv0
+                    }
+            return null
+        }
+
+        if (AllStdBuiltins.some(_ => expr === _ || expr.startsWith(_ + ':')))
+            newval = fileordirstuff(expr)
+
+        if (!newval)
+            return fromProm(vsc.window.showInputBox({ ignoreFocusOut: true, prompt: expr }))
+    }
+    return Promise.resolve<string>(Interpolate(str.slice(0, idx1) + newval + str.slice(idx2 + 1)))
 }
+
+
+
+export const AllStdBuiltins = [
+    Std.workspaceFolder.toString(),
+    Std.workspaceFolderBasename.toString(),
+    Std.file.toString(),
+    Std.relativeFile.toString(),
+    Std.relativeFileDirname.toString(),
+    Std.fileBasename.toString(),
+    Std.fileBasenameNoExtension.toString(),
+    Std.fileDirname.toString(),
+    Std.fileExtname.toString(),
+    Std.cwd.toString(),
+    Std.lineNumber.toString(),
+    Std.selectedText.toString(),
+    Std.execPath.toString(),
+]
